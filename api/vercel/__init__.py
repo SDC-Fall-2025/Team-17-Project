571cae76e36e0de29334d024519b837322b76638
Ethan Meng <uwisc@endfind.me>
2025-10-31 09:48:57 -0500

    ?????
    isn't this virtually the same as 4bb0c86f4e1429d93b7ccbaf4a7ec8d4a1ce4042???
    why is build still failing....

--- api/vercel/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-I serve no purpose except isolate myself alone
-with the eltrich horror that is @vercel/python
-
-oh and also instruct Python to look right above us
-for the real package (which lives in clubradar,
-not here... for obvious reasons.)
-"""
