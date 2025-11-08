2aae6e23f5ae2a23285acf436b4340f6d82fcd18
Ethan Meng <uwisc@endfind.me>
2025-11-08 14:46:14 -0600

    update contribution guide
    i am literally going to faint XP this project is doomed
    to fail from the beginning (what do you think i am, a
    10x developer?  i can't even do 6 people's work.... argh
    this is literally why i hate group projects... why why
    why whyyyy <:c)

--- api/lib/app/__main__.py
+++ api/lib/app/__main__.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument('-d', action='store_true', help='enable debug')
-    parser.add_argument('-b', default='127.0.0.1', help='bind address')
+    parser.add_argument('-b', default='127.0.0.1', help='bind address', metavar='BIND')
     parser.add_argument('port', default='8000', help='listening port', nargs='?')
     args = parser.parse_args()
     App().to_wsgi_app().run(debug=args.d, host=args.b, port=args.port)
