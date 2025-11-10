7aff7441c92f2088115257407e1ecb615e0512db
Ethan Meng <uwisc@endfind.me>
2025-11-10 05:57:46 -0600

    document that --cors flag will need CORS to work

--- api/lib/app/__main__.py
+++ api/lib/app/__main__.py
@@ -1,13 +1,17 @@
 if __name__ == '__main__':
     from . import App
     import argparse
+    import textwrap
 
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument('-d', action='store_true', help='enable debug')
     parser.add_argument('-b', default='127.0.0.1', help='bind address', metavar='BIND')
-    parser.add_argument('--cors', action='store_true', help='allow Cross-Origina Requests')
+    parser.add_argument('--cors', action='store_true', help=textwrap.dedent(
+        "allow Cross-Origina Requests (requires flask-cors, "
+        "which is installed with 'poetry install --with devel')"
+    ))
     parser.add_argument('port', default='8000', help='listening port', nargs='?')
     args = parser.parse_args()
     app = App().to_wsgi_app()
