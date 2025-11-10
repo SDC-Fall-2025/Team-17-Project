743725187a7946ca6a77f7c67909dc373d3db06b
Ethan Meng <uwisc@endfind.me>
2025-11-10 03:35:26 -0600

    Sample search bar from GPT
    so i asked gpt to write me a console log because i didn't
    want to scare people off by telling them to open devtools,
    but ah.... it's hard.  it's fricking hard. :,)  because to
    export OTHER subroutines from a svelte component you need
    to do THIS crazy thing:
    <https://stackoverflow.com/q/58287729/19411800>
    
    ... which i obviously do not have the mental capacity to
    understand.  so here is that done differently anyways.  it's
    cumbersume: the state is stored in debug/conctl.ts, while
    the component is stored in debug/Console.svelte.  but it's
    the only way i can do something as crazy as (ab)using strbuf
    as a global state.... so i guess that's that :/
    
    oh and the backend is absolutely dumb uh i guess they won't
    have a chance to actually connect to any SQLite database
    (but then again they can just json or something for that...
    I'LL DO THAT.  these people just won't do anything on their
    own.....)

--- api/lib/app/__init__.py
+++ api/lib/app/__init__.py
@@ -1,6 +1,8 @@
 import flask
 import json
 
+from werkzeug.exceptions import HTTPException
+
 __all__ = ['App']
 
 class App(object):
@@ -13,12 +15,38 @@ class App(object):
     def greet(self):
         return 'Hello {0[name]}!'.format(self.ctx)
 
+    def search(self, params):
+        # A very bad search function
+        return [f"Hello from {__name__}",
+                "No one has implemented me yet.",
+                "Here are the params I got anyways:",
+                params]
+
     def to_wsgi_app(self):
         app = flask.Flask(self.name)
 
         @app.route('/api/v1/greet')
         def index():
             payload = json.dumps({'message': self.greet(), 'ok': True})
-            return flask.Response(payload, content_type='application_json')
+            return flask.Response(payload, content_type='application/json')
+
+        @app.errorhandler(HTTPException)
+        def handle_http_exception(e):
+            response = e.get_response()
+            response.data = json.dumps({
+                'ok': False,
+                "error": e.name,
+                "description": e.description,
+                "code": e.code,
+            })
+            response.content_type = "application/json"
+            return response
+
+        @app.route('/api/v1/search', methods=['POST'])
+        def search():
+            if not flask.request.is_json:
+                flask.abort(415, description='Need application/json')
+            payload = json.dumps({'results': self.search(flask.request.json), 'ok': True})
+            return flask.Response(payload, content_type='application/json')
 
         return app
